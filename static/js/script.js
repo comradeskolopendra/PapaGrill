a7790146d20d728ebf8cce5e6f0d080488131f9c
DevDima <DCHukhin@logycom.kz>
2023-03-17 16:06:31 +0600

    start a mobile version

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 $(".header__burger").on("click", function() {
     $(".menubar").addClass("menubar__active")
     $(".menubar").toggle("slide")
+    $(".header__burger .header__burger_item").toggleClass("header__burger_item__active")
 })
